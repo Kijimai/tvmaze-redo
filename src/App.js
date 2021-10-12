60bbcf172205a20cba3d7e9fc7454dc56f490e72
Jibbyworks <JDBucog@yahoo.com>
2021-10-12 01:58:56 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//Searchable movies and tv shows
+//click to show more info about the show
+//Save to watch list -- saved on localstorage
+//when movie item is clicked inside the watch list, go to page with remove or rating system
 
-function App() {
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
